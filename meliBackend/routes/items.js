90bc5c47d3eae924daaa073d49a7e1c945a78084
julianfcp <julian.fcp@gmail.com>
2022-07-18 00:25:19 -0500

    itemdetails and refactoring components

--- meliBackend/routes/items.js
+++ meliBackend/routes/items.js
@@ -4,65 +4,64 @@ const router = express.Router();
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-router.get("/query/:query", async (req, res) => {
-  const result = await fetch(
-    `${process.env.URL_MELI_API}/sites/MLA/search?q=${req.params.query}`
-  );
-  const json = await result.json();
+router.get("/", async (req, res) => {
+    const result = await fetch(
+    `${process.env.URL_MELI_API}/sites/MLA/search?q=${req.query.q}`
+    );
+    const json = await result.json();
 
-  const { results, available_filters } = json;
-  if (json.error) {
+    const { results, available_filters } = json;
+    if (json.error) {
     console.log("error!");
     res.send({ error: "Not Found!" });
-  } else {
+    } else {
     const dataResponse = {
-      author: {
+        author: {
         name: process.env.AUTHOR_NAME,
         lastname: process.env.AUTHOR_LASTNAME,
-      },
-      categories: available_filters,
-      items: results,
+        },
+        categories: available_filters,
+        items: results,
     };
     res.send(dataResponse);
-  }
+    }
 });
 router.get("/:id", async (req, res) => {
-  const result = await fetch(
+    const result = await fetch(
     `${process.env.URL_MELI_API}/items/${req.params.id}`
-  );
-  const json = await result.json();
+    );
+    const json = await result.json();
 
-  const descRes = await fetch(
+    const descRes = await fetch(
     `${process.env.URL_MELI_API}/items/${req.params.id}/description`
-  );
-  const jsonDesc = await descRes.json();
-  //const {results, available_filters} = json;
-  if (json.error) {
+    );
+    const jsonDesc = await descRes.json();
+    if (json.error) {
     console.log("error!");
     res.send({ error: "Not Found!" });
-  } else {
+    } else {
     const dataResponse = {
-      author: {
+        author: {
         name: process.env.AUTHOR_NAME,
         lastname: process.env.AUTHOR_LASTNAME,
-      },
-      item: {
+        },
+        item: {
         id: json.id,
         title: json.title,
         price: {
-          currency: json.currency_id,
-          amount: json.price,
-          decimals: "getdecimals Function",
+            currency: json.currency_id,
+            amount: json.price,
+            decimals: "getdecimals Function",
         },
-      },
-      picture: json.pictures[0].url,
-      condition: json.condition,
-      free_shipping: json.shipping.free_shipping,
-      sold_quantity: json.sold_quantity,
-      description: jsonDesc.plain_text,
+        },
+        picture: json.pictures[0].url,
+        condition: json.condition,
+        free_shipping: json.shipping.free_shipping,
+        sold_quantity: json.sold_quantity,
+        description: jsonDesc.plain_text,
     };
     res.send(dataResponse);
-  }
+    }
 });
 
 module.exports = router;
